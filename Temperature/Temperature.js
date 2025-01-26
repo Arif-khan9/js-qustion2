b3640c986a0d7effab1acd04deb4e0cea16f0a78
Arif khan <ak4040651@gmail.com>
2025-01-26 18:10:38 +0530

    Temperature

---
+++ Temperature/Temperature.js
@@ -0,0 +1,10 @@
+// 10.	Temperature Converter:
+// Question: Create a program that converts temperature between Celsius and Fahrenheit based on user input.
+// Example Input:
+
+
+const celsiusToFahrenheit = (celsius) =>{
+    return (celsius *9) / 5+32;
+
+};
+console.log("Result",celsiusToFahrenheit(25));
