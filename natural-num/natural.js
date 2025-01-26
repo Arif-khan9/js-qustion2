d86dffd9793090b0a21b21e172029c20df4759cd
Arif khan <ak4040651@gmail.com>
2025-01-26 17:18:47 +0530

    natural-num

---
+++ natural-num/natural.js
@@ -0,0 +1,11 @@
+// 3.	Sum of Natural Numbers:
+// Question: Write a program to calculate the sum of all natural numbers up to a given number ￼.
+// Example Input:
+
+let num = parseInt(prompt("Enter the number"));
+let sum = 0;
+for(let i=1;i<=num;i++){
+    sum=sum+i;
+
+}
+console.log(sum);
