2e6c136598b31e375ef9a7742c7bbc79c6d81975
Arif khan <ak4040651@gmail.com>
2025-01-26 18:21:55 +0530

    factorial-num

---
+++ factorial-num/factorial-num.js
@@ -0,0 +1,10 @@
+//1.  Question: Write a program that calculates the factorial of a given number ￼.
+// Example Input:
+
+
+let number = 10;
+let factorialValue = 1;
+for(let i = 1;i<=number;i++){
+    factorialValue = factorialValue*i;
+    console.log( 'factorial value' +number+'=', factorialValue); 
+}
