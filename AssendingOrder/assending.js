e118abf6cfe5c806b9cbedb87c64a6134ef77e43
Arif-khan9 <ak4040651@gmail.com>
2026-03-03 15:27:07 +0530

    assending order solve

---
+++ AssendingOrder/assending.js
@@ -0,0 +1,14 @@
+
+// assendig order
+
+function isArraySorted(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < arr[i - 1]) {
+            return false;   
+        }
+    }
+    return true;
+}
+
+
+console.log(isArraySorted([4,2,3,4]));
