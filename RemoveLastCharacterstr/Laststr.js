3cea36fabbb35c469be0cee9ff94014bb7671952
Arif-khan9 <ak4040651@gmail.com>
2026-04-25 23:02:13 +0530

    FilterPositiveNum solve

--- RemoveLastCharacterstr/Laststr.js
+++ RemoveLastCharacterstr/Laststr.js
@@ -1,2 +1,14 @@
 // Remove Last Character from a String
 
+function removeLastChar(str){
+  let result = "";
+
+  for(let i = 0; i < str.length - 1; i++){
+    result += str[i];
+  }
+
+  return result;
+}
+
+let output = removeLastChar("hello");
+console.log(output);
