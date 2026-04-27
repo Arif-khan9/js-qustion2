90b8bc420cee1776b67bda01307c573e491ae8f6
Arif-khan9 <ak4040651@gmail.com>
2026-04-27 22:17:32 +0530

    count space string solve

--- FilterPositiveNum/FilterPositive.js
+++ FilterPositiveNum/FilterPositive.js
@@ -1,2 +1,16 @@
 // Filter Positive Numbers from an Array
 
+function getPositive(arr){
+  let result = [];
+
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] > 0){
+      result.push(arr[i]);
+    }
+  }
+
+  return result;
+}
+
+let output = getPositive([1,-2,3,-4,5]);
+console.log(output);
