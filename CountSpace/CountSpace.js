e276545bf419b4319409db503d33999276a3ddf7
Arif-khan9 <ak4040651@gmail.com>
2026-04-28 20:23:13 +0530

    reomove vawels string

--- CountSpace/CountSpace.js
+++ CountSpace/CountSpace.js
@@ -1,3 +1,17 @@
 // Count Spaces in a String
 
 
+function countSpaces(str){
+  let count = 0;
+
+  for(let i = 0; i < str.length; i++){
+    if(str[i] === " "){
+      count++;
+    }
+  }
+
+  return count;
+}
+
+let output = countSpaces("my name is arif");
+console.log(output);
