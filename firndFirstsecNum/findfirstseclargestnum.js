e515e9b8013e8702abb29e2df8197f1a8bc82971
Arif-khan9 <ak4040651@gmail.com>
2026-05-03 21:51:25 +0530

    JoinArrayofStrings solve

--- firndFirstsecNum/findfirstseclargestnum.js
+++ firndFirstsecNum/findfirstseclargestnum.js
@@ -1,2 +1,21 @@
 // Find Second Largest Number in an Array
 
+function secondLargest(arr){
+  let first = -Infinity;
+  let second = -Infinity;
+
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] > first){
+      second = first;
+      first = arr[i];
+    } 
+    else if(arr[i] > second && arr[i] !== first){
+      second = arr[i];
+    }
+  }
+
+  return second;
+}
+
+let result = secondLargest([5,1,9,3]);
+console.log(result);
