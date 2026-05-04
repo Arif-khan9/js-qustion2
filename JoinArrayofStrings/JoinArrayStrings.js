4eca6bdd6314fb0f6ebb6924a3819bbfb7a5a77f
Arif-khan9 <ak4040651@gmail.com>
2026-05-04 12:07:52 +0530

    LeftRotateArray solve

--- JoinArrayofStrings/JoinArrayStrings.js
+++ JoinArrayofStrings/JoinArrayStrings.js
@@ -1,1 +1,19 @@
 // Join Array of Strings into a Single String
+
+
+function joinStrings(arr){
+  let result = "";
+
+  for(let i = 0; i < arr.length; i++){
+    result += arr[i];
+
+    if(i !== arr.length - 1){
+      result += " ";
+    }
+  }
+
+  return result;
+}
+
+let output = joinStrings(["hello", "world", "js"]);
+console.log(output);
