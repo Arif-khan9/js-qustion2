c399f1740e9f524ad42dd112514b6af5e55248f6
Arif-khan9 <ak4040651@gmail.com>
2026-05-07 14:30:26 +0530

    DifferenceBetweenLargest solve

---
+++ DifferenceBetweenLargest/DifferenceBetweenLargest.js
@@ -0,0 +1,23 @@
+// Find Difference Between Largest and Smallest Number in Array
+
+
+function findDifference(arr){
+  let max = arr[0];
+  let min = arr[0];
+
+  for(let i = 1; i < arr.length; i++){
+
+    if(arr[i] > max){
+      max = arr[i];
+    }
+
+    if(arr[i] < min){
+      min = arr[i];
+    }
+  }
+
+  return max - min;
+}
+
+
+console.log(findDifference([4,9,1,7]));
