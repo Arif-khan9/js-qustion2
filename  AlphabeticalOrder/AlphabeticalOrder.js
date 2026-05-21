86f2788fe2f4cb9bb52d25a021c2815b04969e4c
Arif-khan9 <ak4040651@gmail.com>
2026-05-21 15:46:35 +0530

    lphabeticalOrder solve using fun

---
+++  AlphabeticalOrder/AlphabeticalOrder.js
@@ -0,0 +1,12 @@
+// ⁠JavaScript Program to Sort Words in Alphabetical Order. 
+// 24
+
+
+function  Alphabetical(arr){
+
+    let store = arr.sort()
+   
+    return store
+
+}
+console.log(Alphabetical(["Banana", "Apple", "Mango"]))
