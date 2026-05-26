c1f799d1426b78761a32da80da8d4f9f17cddebc
Arif-khan9 <ak4040651@gmail.com>
2026-05-26 15:05:31 +0530

    Particulerproperty solve

---
+++ Particulerproperty/Particulerproperty.js
@@ -0,0 +1,15 @@
+// JavaScript Program to Check if a Key Exists in an Object.
+
+let person = {
+  name: "Arif",
+  age: 21
+}
+
+   let store = person.hasOwnProperty("city")
+   console.log(store);
+   
+
+// console.log(person.hasOwnProperty("age")) // true
+// console.log(person.hasOwnProperty("city")) // false
+
+
