0a58876969ce9377ba1d7dbfb46336dc8b5c7da1
Arif-khan9 <ak4040651@gmail.com>
2026-06-03 16:38:09 +0530

    MargetwoObject solve

--- ProgramClone/program.js
+++ ProgramClone/program.js
@@ -1,20 +1,11 @@
 // javascript program to clone a js object
-// using Object.assign() method
 
-// creating a js object
-const obj1 = {
-    name: "John",
-    age: 30,
-    city: "New York"
+
+const person = {
+  name: "Arif",
+  age: 21
 };
 
-// cloning the object using Object.assign() method
-const obj2 = Object.assign({}, obj1);
+const clone = { ...person };
 
-// displaying the original and cloned objects
-console.log("Original Object:", obj1);
-console.log("Cloned Object:", obj2);
-
-// modifying the cloned object
-obj2.name = "Jane";
-obj2.age = 25;
+console.log(clone);
