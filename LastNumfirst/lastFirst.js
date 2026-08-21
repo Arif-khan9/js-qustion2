506bb1d71acdd4410f6e4a549015801a896e0905
Arif-khan9 <ak4040651@gmail.com>
2026-08-21 16:29:14 +0530

    solve this qustion without using for arr method

---
+++ LastNumfirst/lastFirst.js
@@ -0,0 +1,14 @@
+
+
+let arr = [1,2,3,4,5]
+
+function lastFirst(arr){
+    let last = arr[arr.length-1]
+    for(let i=arr.length-1;i>0;i--){
+       arr[i]=arr[i-1]
+    }
+    arr[0]=last
+    return arr
+   
+}
+console.log(lastFirst(arr))
